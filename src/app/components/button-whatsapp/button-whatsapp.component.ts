f9c43e062aa8deee873084bc33883c44773dbdf5
Maxi <maxi.marchi@hotmail.com>
2025-05-12 11:04:22 -0300

    se cambia color base

--- src/app/components/button-whatsapp/button-whatsapp.component.ts
+++ src/app/components/button-whatsapp/button-whatsapp.component.ts
@@ -11,7 +11,7 @@ import { Subject, takeUntil } from 'rxjs';
 </a>
 
 <a href="https://wa.me/+5491145280955?text=Hola%20quiero%20información" target="_blank"  *ngIf="showCartel" class="mensaje fadeIn d-flex">
-<svg width="50" height="50" fill="#ea7306" class="bi bi-chat-dots-fill mr-3" viewBox="0 0 16 16">
+<svg width="50" height="50" fill="#b91828" class="bi bi-chat-dots-fill mr-3" viewBox="0 0 16 16">
   <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7M5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0m4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0m3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
 </svg>
 <p><b>¿Tenés dudas?</b> Mandanos un mensaje</p>
