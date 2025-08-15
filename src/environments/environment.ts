710fdf8b2c7c1168cc9dd7082efe6e7c90e09f27
Maxi <maxi.marchi@hotmail.com>
2025-08-15 15:00:22 -0300

    se cambia la url del back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   //baseUrl: 'http://localhost:3000/api',
-  baseUrl: 'https://seguros-back-8oyh5xbdq-mogensens-projects.vercel.app/api',
+  baseUrl: 'https://seguros-back.vercel.app/api',
   production: false
 };
