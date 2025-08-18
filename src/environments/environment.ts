e388418e99af3d6992b8bb05178e132026adfd6b
nicom <nicombook@outlook.com>
2025-08-18 09:55:49 -0300

    - Mergeo con la rama Main 18082025.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  baseUrl: 'http://localhost:3000/api',
+  //baseUrl: 'http://localhost:3000/api',
+  baseUrl: 'https://seguros-back.vercel.app/api',
   production: false
 };
